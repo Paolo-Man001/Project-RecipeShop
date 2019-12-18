6b1813b20c4d7bb944140d1af179d4d2a2d26b44
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-12-18 16:06:16 +1300

    Create layout to Add recipe-list and recipe-item using the recipe model to add Content to Recipe Component.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RecipeModel} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: RecipeModel[] = [
+    // tslint:disable-next-line:max-line-length
+    new RecipeModel('A test recipe', 'A simple test', 'https://assets.epicurious.com/photos/59e8f2efd0709e43c9418b02/1:1/w_400%2Cc_limit/monkfish-and-cauliflower-chowder-recipe-BA-101917.jpg')
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
